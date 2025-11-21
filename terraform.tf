dcaf522b457188385f03531aeda8c7438a6b9165
Benjamin Bennett <benjamin.bennett@digital.cabinet-office.gov.uk>
2025-11-21 14:15:27 +0000

    Migrating terraform state file to remote S3 backend

--- terraform.tf
+++ terraform.tf
@@ -6,5 +6,12 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket = "ndx-try-tf-state"
+    key = "state/terraform.tfstate"
+    region = "us-west-2"
+    
+  }
+
   required_version = ">= 1.14"
 }
